d74fa15e362b71a855ef3316d83a3308eeaf81b5
goodleaf96 <ky1234tho@gmail.com>
2025-02-26 09:42:18 +0900

    Initial commit

---
+++ src/main/java/com/example/kjy_test_backend/KjyTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.kjy_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjyTestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KjyTestBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kjy_test_backend/KjyTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kjy_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjyTestBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
